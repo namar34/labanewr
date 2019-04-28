d6ca1a98d17258dd12dad20bd41d5f7e2f7dfca4
Namar <skidanroma@yandex.ru>
2019-04-28 22:10:51 +0400

    funct

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <sdt.h>
  int main()
  {
+int a, b;
+ cout << "Enter A and B: ";
+ cin >> a >> b;
+cout << "A + B = " << a + b << '\n'
+ << "A- B = " << a - b << '\n'
+ << "A * B = " << a * b << '\n';
  }
